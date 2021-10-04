e56792e51c2b58653b360341c728d2f9e46cc817
Nandini Rajaram <nandinirajaram@gmail.com>
2021-10-04 15:35:59 -0500

    cucumber setup commit

---
+++ gitcucumber/src/main/java/gitcucumber/gitcucumber/App.java
@@ -0,0 +1,13 @@
+package gitcucumber.gitcucumber;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
